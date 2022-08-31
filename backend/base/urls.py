82497f9a892ea96ae1781b185b768ea1d0e5f9cf
Alexander Lucano <alrlucano@gmail.com>
2022-08-30 23:38:51 -0500

    Agregamos permisos de visualizacion para ver usuarios , creamos el metodo para crear usuario , falta el template

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -7,10 +7,11 @@ from . import views
 urlpatterns = [
     #autentication
     path('users/login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
-
-    path('', views.getRoutes, name='get-routes'),
+    path('users/register/',views.registerUser,name='register'),
+    
 
     path('users/profile/',views.getUserProfile,name='user-profile'),
+    path('users/',views.getUsers,name='users'),
 
     path('products/', views.getProducts, name='get-products'),
     path('products/<str:pk>/', views.getProduct, name='get-product'),
--- backend/base/views.py
+++ backend/base/views.py
@@ -1,9 +1,16 @@
 
 from django.shortcuts import render
-from django.http import JsonResponse
-from rest_framework.decorators import api_view
+
+from rest_framework.decorators import api_view,permission_classes
+from rest_framework.permissions import IsAuthenticated,IsAdminUser
+
 from rest_framework.response import Response
+
+
+
 from .models import Product
+from django.contrib.auth.models import User
+
 from .serializers import ProductSerializer, UserSerializer,UserSerializerWithToken
 
 # Create your views here.
@@ -12,6 +19,8 @@ from .serializers import ProductSerializer, UserSerializer,UserSerializerWithTok
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 
+from django.contrib.auth.hashers import make_password
+from rest_framework import status
 
 #ESTO LO VE EL BACKEND en el rest
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
@@ -31,24 +40,24 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
 
-@api_view(['GET'])
-def getRoutes(request):
-    routes=[
-        '/api/products',
-        '/api/products/create/',
 
-        '/api/products/upload/',
-
-        '/api/products/<id>/reviews/',
-
-        '/api/products/top/',
-        '/api/products/<id>/',
-
-        '/api/products/delete/<id>/',
-        '/api/products/<update>/<id>/',
-    ]
-    return Response(routes,)
-
+@api_view(['POST'])
+def registerUser(request):
+    data=request.data
+    try:
+        user=User.objects.create(
+            first_name=data['name'],
+            username=data['email'],
+            email=data['email'],
+            password=make_password(data['password']),
+            
+        )
+        serializer=UserSerializerWithToken(user,many=False)
+        return Response(serializer.data)
+    except:
+        message ={'detail':'User with this email already exists'}
+        return Response(message,status=status.HTTP_400_BAD_REQUEST)
+    
 
 #recordar esto al IMPLEMENTAR:
 #probamos el usuario desde el rest de django para obtener un token y tener el access:
@@ -56,13 +65,21 @@ def getRoutes(request):
 #probamos con postman en este caso revisar collections
 #esto lo ve el frontend 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def getUserProfile(request):
     #gracias a la API de token el request.user devolvera varios valores y no solo el user
-    user = request.user
-    
+    user = request.user    
     #estamos mostrando varios productos por tanto el many va true
     serializer=UserSerializer(user,many=False)
     return Response(serializer.data)
+
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def getUsers(request):
+    users=User.objects.all()
+    #estamos mostrando varios productos por tanto el many va true
+    serializer=UserSerializer(users,many=True)
+    return Response(serializer.data)    
 
 @api_view(['GET'])
 def getProducts(request):

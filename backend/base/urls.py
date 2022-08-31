e546802a4000cb29782e080d9ef699ac812ab1a1
Alexander Lucano <alrlucano@gmail.com>
2022-08-30 22:04:34 -0500

    Agregamos JWT para la autenticacion y obtener tokens , revisar el uso de serializer y como editamos campos para la informacion en views y en serializers.py

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
 from . import views
+# from rest_framework_simplejwt.views import (
+#     TokenObtainPairView,    
+# )
 
 urlpatterns = [
+    #autentication
+    path('users/login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+
     path('', views.getRoutes, name='get-routes'),
+
+    path('users/profile/',views.getUserProfile,name='user-profile'),
+
     path('products/', views.getProducts, name='get-products'),
     path('products/<str:pk>/', views.getProduct, name='get-product'),
 ]
--- backend/base/views.py
+++ backend/base/views.py
@@ -1,11 +1,35 @@
+
 from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Product
-from .serializers import ProductSerializer
+from .serializers import ProductSerializer, UserSerializer,UserSerializerWithToken
 
 # Create your views here.
+
+#TOKENS
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+
+#ESTO LO VE EL BACKEND en el rest
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        serializer= UserSerializerWithToken(self.user).data
+
+        for k,v in serializer.items():
+            data[k]=v
+
+        # data['username'] = self.user.username
+        # data['email'] = self.user.email
+
+        return data
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
 
 @api_view(['GET'])
 def getRoutes(request):
@@ -24,6 +48,21 @@ def getRoutes(request):
         '/api/products/<update>/<id>/',
     ]
     return Response(routes,)
+
+
+#recordar esto al IMPLEMENTAR:
+#probamos el usuario desde el rest de django para obtener un token y tener el access:
+#con esto ya podemos hacer recien las pruebas y obtener datos de la base de datos
+#probamos con postman en este caso revisar collections
+#esto lo ve el frontend 
+@api_view(['GET'])
+def getUserProfile(request):
+    #gracias a la API de token el request.user devolvera varios valores y no solo el user
+    user = request.user
+    
+    #estamos mostrando varios productos por tanto el many va true
+    serializer=UserSerializer(user,many=False)
+    return Response(serializer.data)
 
 @api_view(['GET'])
 def getProducts(request):

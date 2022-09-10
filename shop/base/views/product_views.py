72986311c12aa51d6fb420b8df43c50c4f740cbf
Alexander Lucano <alrlucano@gmail.com>
2022-09-10 17:16:47 -0500

    Error fixed

--- shop/base/views/product_views.py
+++ shop/base/views/product_views.py
@@ -166,18 +166,3 @@ def createProductReview(request,pk):
         product.save()
         return Response('Review Added')
 
-from django.views import View
-from django.http import HttpResponse, HttpResponseNotFound
-import os
-
-# Add this CBV
-class Assets(View):
-
-    def get(self, _request, filename):
-        path = os.path.join(os.path.dirname(__file__), 'static', filename)
-
-        if os.path.isfile(path):
-            with open(path, 'rb') as file:
-                return HttpResponse(file.read(), content_type='application/javascript')
-        else:
-            return HttpResponseNotFound()
